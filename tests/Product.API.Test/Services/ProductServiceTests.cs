b955b10ce19d927ef3e8df09adc8f9575acb2a3b
AlefT94 <alef_teodoro1@hotmail.com>
2025-02-21 12:15:08 -0300

    test: implemented ProductService tests

---
+++ tests/Product.API.Test/Services/ProductServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using NSubstitute;
+using Products.API.Bus;
+using Products.API.Data;
+using Products.API.Services;
+
+namespace Product.API.Test.Services;
+public class ProductServiceTests
+{
+    private readonly AppDbcontext _dbContext;
+    private readonly IBusService _bus;
+    private readonly ProductService _productService;
+
+    public ProductServiceTests()
+    {
+        _dbContext = Substitute.For<AppDbcontext>();
+        _bus = Substitute.For<IBusService>();
+        _productService = new ProductService(_dbContext, _bus);
+    }
+
+
+}
