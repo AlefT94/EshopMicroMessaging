786bcb56242e960a3b6a0142e0a7d29caf4e6650
AlefT94 <alef_teodoro1@hotmail.com>
2025-02-27 12:58:25 -0300

    feat: create first repository mthods implementation

---
+++ src/Inventory.API/Data/Repository/IInventoryItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using Inventory.API.Entities;
+
+namespace Inventory.API.Data.Repository;
+
+public interface IInventoryItemRepository
+{
+    public Task<bool> Create(InventoryItem inventoryItem);
+    public Task<bool> Delete(Guid inventoryItemId);
+    public Task<InventoryItem?> GetByProductId(Guid productId);
+    public Task<IEnumerable<InventoryItem>> GetAll();
+    public Task<bool> Update(InventoryItem inventoryItem);
+}
---
+++ src/Inventory.API/Data/Repository/InventoryItemRepository.cs
@@ -0,0 +1,33 @@
+﻿using Inventory.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.API.Data.Repository;
+
+public class InventoryItemRepository(AppDbcontext dbContext) : IInventoryItemRepository
+{
+    public async Task<bool> Create(InventoryItem inventoryItem)
+    {
+        await dbContext.AddAsync(inventoryItem);
+        return await dbContext.SaveChangesAsync() > 0;
+    }
+
+    public Task<bool> Delete(Guid inventoryItemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<InventoryItem>> GetAll()
+    {
+        return await dbContext.InventorieItems.ToListAsync();
+    }
+
+    public Task<InventoryItem?> GetByProductId(Guid productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(InventoryItem inventoryItem)
+    {
+        throw new NotImplementedException();
+    }
+}
